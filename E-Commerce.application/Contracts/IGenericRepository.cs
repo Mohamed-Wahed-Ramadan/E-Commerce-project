917773773866cbd306d065f582a1e2d6d911e075
Mohmed Kamal <mohamed.elsharif022@gmail.com>
2025-09-12 21:19:56 +0300

    Added genericRepository Functionality and Added User configurations

--- E-Commerce.application/Contracts/IGenericRepository.cs
+++ E-Commerce.application/Contracts/IGenericRepository.cs
@@ -8,5 +8,12 @@ namespace E_Commerce.application.Contracts
 {
     public interface IGenericRepository<T,TID>
     {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T?> GetByIdAsync(TID id);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+
+        Task<int> CompleteAsync();
     }
 }
--- E-commerce.infratructure/GenericRepository.cs
+++ E-commerce.infratructure/GenericRepository.cs
@@ -1,5 +1,7 @@
 ﻿using E_Commerce.application.Contracts;
+using E_Commerce.context;
 using E_Commerce_project.models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,8 +10,42 @@ using System.Threading.Tasks;
 
 namespace E_commerce.infratructure
 {
-    public class GenericRepository<T,TID> : IGenericRepository<T, TID> where T : BaseModel<TID>
+    public class GenericRepository<T, TID> : IGenericRepository<T, TID> where T : BaseModel<TID>
     {
+        private readonly AppDbContext _appDbContext;
+        public GenericRepository(AppDbContext dbContext)
+        {
+           _appDbContext = dbContext;   
+        }
+        public void Add(T entity)
+        {
+            _appDbContext.Add(entity);
+        }
 
+        
+
+        public void Delete(T entity)
+        {
+            _appDbContext.Remove(entity);
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _appDbContext.Set<T>().ToListAsync();
+        }
+
+        public async Task<T?> GetByIdAsync(TID id)
+        {
+            return await _appDbContext.FindAsync<T>(id);
+        }
+
+        public void Update(T entity)
+        {
+            _appDbContext.Update(entity);
+        }
+        public Task<int> CompleteAsync()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
