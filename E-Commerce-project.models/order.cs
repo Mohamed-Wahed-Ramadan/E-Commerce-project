0b562969917e7c9539a3644f968509a832a63955
Ahemd Hani <ahmed.h.m112001@gmail.com>
2025-09-13 17:21:16 +0300

    update

--- E-Commerce-project.models/order.cs
+++ E-Commerce-project.models/order.cs
@@ -9,7 +9,7 @@ namespace E_Commerce_project.models
     public class order : BaseModel<int>
     {
         public List<ProductOrder> ProductOrder { get; set; }
-        public user User { get; set; }
+        //public User User { get; set; }
         public decimal OrderTotalPrice { get; set; }
         public DateTime OrderDate { get; set; }
         public DateTime ReceiptDate { get; set; }
