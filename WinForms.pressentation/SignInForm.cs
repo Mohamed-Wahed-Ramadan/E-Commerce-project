833c7f8991ac976f899059e679fcf8960ce5f7bb
Mohamed-Wahed <medo03459@gmail.com>
2025-09-16 11:24:29 +0300

    new changes

--- WinForms.pressentation/SignInForm.cs
+++ WinForms.pressentation/SignInForm.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using Autofac;
+using E_commerce.infratructure;
+using E_Commerce.application.Interfaces;
+using E_Commerce.application.Services;
+using E_Commerce.DTOs.User;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +17,14 @@ namespace WinForms.pressentation
 {
     public partial class SignInForm : Form
     {
+        private readonly UserService _userService;
+
         public SignInForm()
         {
             InitializeComponent();
+            var builder = Autofac.Inject();
+            _userService = (UserService?)builder.Resolve<IUserServices>();
+           
         }
 
         private void label4_Click(object sender, EventArgs e)
@@ -29,11 +39,32 @@ namespace WinForms.pressentation
             form.Show();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private async void button1_Click(object sender, EventArgs e)
         {
-            LoginForm loginForm = new LoginForm();
-            this.Hide();
-            loginForm.Show();
+            var newUser = new CreateUserDTO
+            {
+                UserName = txtName.Text.Trim(),
+                Email = txtEmail.Text.Trim(),
+                Password = txtPassword.Text.Trim()
+            };
+
+            // 2) استدعاء RegisterAsync
+            var (success, message) = await _userService.RegisterAsync(newUser);
+
+            // 3) التعامل مع النتيجة
+            if (success)
+            {
+                MessageBox.Show(message, "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                // بعد التسجيل يروح على الـ Login
+                LoginForm loginForm = new LoginForm();
+                this.Hide();
+                loginForm.Show();
+            }
+            else
+            {
+                MessageBox.Show(message, "Register Failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
